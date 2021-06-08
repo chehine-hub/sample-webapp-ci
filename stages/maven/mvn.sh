7e719218a67c8b95f232a60d5091a013101786ea
Chehine <chehine_marouani@hotmail.com>
2021-06-08 14:39:56 -0400

    Update mvn.sh

--- stages/maven/mvn.sh
+++ stages/maven/mvn.sh
@@ -4,4 +4,4 @@ echo "****************************"
 echo "******* Building WAR *******"
 echo "****************************"
 
-docker run --rm -v /root/jenkins-agent/workspace/maven-project/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-openjdk-11 "$@"
+docker run --rm -v /root/jenkins-agent/workspace/Sample-WebApp-CI/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-openjdk-11 "$@"

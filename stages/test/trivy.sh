4db20005399006f700bab2c3b66b3cd59e70ca39
Chehine <chehine_marouani@hotmail.com>
2021-06-08 15:37:23 -0400

    Update trivy.sh

--- stages/test/trivy.sh
+++ stages/test/trivy.sh
@@ -4,4 +4,4 @@ echo "***********************************"
 echo "******** Scan Docker Image ********"
 echo "***********************************"
 
-trivy image --exit-code 0 maven-project-$ENV:$BUILD_TAG 
+trivy image --exit-code 1 maven-project-$ENV:$BUILD_TAG 
